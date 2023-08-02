15c63fbf6971d1767e03ffc670e89dedcc5ae6e3
Felipe Uliana <felipevendramin@ext.cvccorp.com.br>
2023-08-02 18:15:03 -0300

    fix: :hammer: fix modifyComponentExport action

--- .plop/actions/modify-component-export.mjs
+++ .plop/actions/modify-component-export.mjs
@@ -2,7 +2,7 @@ export default function modifyComponentExport (basePath) {
   return {
     path: `${basePath}/index.ts`,
     pattern: /(\/\/ COMPONENT EXPORTS)/g,
-    template: ',\n  {{pascalCase name}}\n$1',
+    template: ',\n  {{pascalCase name}}$1',
     type: 'modify'
   }
 }
